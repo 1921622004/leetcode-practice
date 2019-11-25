d1a5dc8acd1c96d8d8aefdda1f6628078d347dee
1921622004 <1921622004qwer@gmail.com>
2019-11-25 21:24:31 +0800

    32

--- 32.最长有效括号.js
+++ 32.最长有效括号.js
@@ -9,8 +9,19 @@
  * @param {string} s
  * @return {number}
  */
-var longestValidParentheses = function(s) {
-    
+var longestValidParentheses = function (s) {
+  const queue = [-1];
+  let max = 0;
+  for (let i = 0; i < s.length; i++) {
+    if (s[queue[queue.length - 1]] === '(' && s[i] === ')') {
+      queue.pop();
+      let length = i - queue[queue.length - 1];
+      max = Math.max(length, max);
+    } else {
+      queue.push(i)
+    }
+  }
+  return max
 };
 // @lc code=end
 
