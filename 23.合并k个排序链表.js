f8223097283bddc63e20c10256e0249848e97bd9
1921622004 <1921622004qwer@gmail.com>
2019-12-12 23:44:38 +0800

    23

--- 23.合并k个排序链表.js
+++ 23.合并k个排序链表.js
@@ -14,7 +14,29 @@
  * @param {ListNode[]} lists
  * @return {ListNode}
  */
-var mergeKLists = function(lists) {
-    
+var mergeKLists = function (lists) {
+  if (lists.length === 1) return lists[0];
+  let res = new ListNode(null);
+  let temp = res;
+  while (lists.length) {
+    let min = Number.MAX_SAFE_INTEGER;
+    let minIdx = -1;
+    for (let i = 0; i < lists.length; i++) {
+      if (!lists[i]) continue
+      if (lists[i].val <= min) {
+        minIdx = i;
+        min = lists[i].val
+      }
+    }
+    if (minIdx === -1) break;
+    temp.next = lists[minIdx];
+    if (lists[minIdx].next) {
+      lists[minIdx] = lists[minIdx].next;
+    } else {
+      lists.splice(minIdx, 1)
+    }
+    temp = temp.next;
+  }
+  return res.next;
 };
 
